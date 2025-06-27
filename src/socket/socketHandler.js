e3b0ec78d41853a0e8504437c9b6eb06089e71c2
Pranjal Satti <pranjalsatti@gmail.com>
2025-06-27 19:30:07 +0530

    add socket communication

--- src/socket/socketHandler.js
+++ src/socket/socketHandler.js
@@ -1,26 +1,41 @@
-import { Message } from "../models/message.model";
-import { ApiError } from "../utils/apiError";
+import { ChatRoom } from "../models/chatRoom.model.js";
+import { Message } from "../models/message.model.js";
+
 
 
 export const socketHandler=async(socket,io)=>{
     console.log("new user connected",socket.id)
 
     socket.on("joinRoom",async({userId,roomId})=>{
-        socket.join(roomId);
-        console.log("user join room",userId,roomId)
-
-        io.to(roomId).emit("userJoined",{userId})
-    })
+        try {
+            socket.join(roomId);
+            console.log("user join room",userId,roomId)
+    
+            await ChatRoom.findByIdAndUpdate(roomId,
+                {
+                    $addToSet:{
+                        members:userId
+                    },
+                },
+                {new:true}
+            )
+            io.to(roomId).emit("userJoined",{userId})
+        } catch (err) {
+            console.error("Socket error:", err.message);
+            socket.emit("errorOccurred", { message: "fail to join room" });
+            
+        }
+    });
 
     socket.on("sendMessage",async({roomId,senderId,file,fileType,content})=>{
-        if(!roomId||!senderId||!file||!fileType||!content){
-            throw new ApiError(400,"all field required")
-        }
-        const message=await Message.create({chatRoom:roomId,sender:senderId,file,fileType,content})
-
-        const populate=(await message.populate("chatRoom","name members")).populate("sender","name avatar")
-        io.to(roomId).emit("receiveMessage",{populate})
-    })
-
+        try {
+            const message=await Message.create({chatRoom:roomId,sender:senderId,file,fileType,content})
     
+            const populated = await message.populate([{ path: "chatRoom", select: "name participants" },{ path: "sender", select: "name avatar" }]);
+            io.to(roomId).emit("receiveMessage",populated)
+        } catch (error) {
+            console.error("sendMessage error:", err.message);
+            socket.emit("errorOccurred", { message: "Failed to send message." })   
+        }
+    })   
 }
